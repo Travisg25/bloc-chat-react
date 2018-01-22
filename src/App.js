ba6dd1139440bffacf1e66bea9fc0b95d54ca2f2
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-01-22 10:31:45 -0800

    usernames added to screen, users able to diaply name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList  from './components/RoomList.js';
 import MessageList from './components/MessageList.js';
+import User  from './components/User.js';
+
 
 var config = {
   apiKey: "AIzaSyCL6--NBUm0JYiTh4aXLYGSW6Wt80ParQs",
@@ -22,10 +23,11 @@ class App extends Component {
     super(props);
     this.state = {
       activeRoom: "",
-      user: ""
+      user: null
     };
     this.setActiveRoom = this.setActiveRoom.bind(this);
     this.setUser = this.setUser.bind(this);
+
   }
 
 setActiveRoom(room) {
@@ -38,9 +40,11 @@ setUser(user) {
 
   render() {
     let showMessages = this.state.activeRoom;
+    let currentUser = this.state.user === null ? "Guest" : this.state.user.displayName;
 
     return (
       <div>
+        <User firebase={firebase} setUser={this.setUser} currentUser={currentUser}/>
         <h1>{this.state.activeRoom.name || "Choose a room or Create one"}</h1>
         <RoomList firebase={firebase} setActiveRoom={this.setActiveRoom} />
         { showMessages ?
