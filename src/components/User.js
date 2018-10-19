7c59847f982680497696a13e4dcb47a346e0d6e4
Travis Grossman <TravisGrossman@Traviss-iMac.local>
2018-10-19 09:23:22 -0700

    user and user css edit

--- src/components/User.js
+++ src/components/User.js
@@ -1,47 +1,78 @@
-import React, { Component } from 'react';
-import * as firebase from 'firebase';
-import '.././styles/user.css';
-import {Col, Row, Button}  from 'react-bootstrap';
+import React, { Component } from "react";
+import ".././styles/user.css";
 
 class User extends Component {
-  constructor (props){
+  constructor(props) {
     super(props);
     this.signIn = this.signIn.bind(this);
     this.signOut = this.signOut.bind(this);
   }
 
   componentDidMount() {
-    this.props.firebase.auth().onAuthStateChanged((user) => {
+    this.props.firebase.auth().onAuthStateChanged(user => {
       this.props.setUser(user);
     });
   }
 
   signIn() {
-  const provider = new this.props.firebase.auth.GoogleAuthProvider();
-  this.props.firebase.auth().signInWithPopup(provider).then((result) => {
-    const user = result.user;
-    this.props.setUser(user);
-  });
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(result => {
+        const user = result.user;
+        this.props.setUser(user);
+      });
   }
 
+  /*  signOut() {
+    this.props.firebase.auth().onAuthStateChanged(user => {
+      if (user !== null) {
+        const userRef = this.props.firebase
+          .database()
+          .ref("presence/" + user.uid);
+        userRef.update({ isOnline: false, currentRoom: "", roomName: "" });
+      }
+    });
+    this.props.firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.props.setUser(null);
+      });
+  }
+*/
+
   signOut() {
-  this.props.firebase.auth().signOut().then(() => {
-    this.props.setUser(null);
-  });
+    this.props.firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.props.setUser(null);
+      });
   }
   render() {
-     return (
-       <Row className="loginSection">
-        <Col xs={12} className="loginSection">
-          <h5>Welcome, {this.props.currentUser}</h5>
-          { this.props.currentUser === "Guest" ?
-            <h6 onClick={this.signIn}>Sign In</h6>
-            :
-            <h6 onClick={this.signOut}>Sign Out</h6>
-          }
-        </Col>
-      </Row>
-    )
+    return (
+      <div className="login-section">
+        <h5 className="welcome-header">{this.props.welcome}</h5>
+        {this.props.welcome === "Guest" ? (
+          <h6
+            className="cursor-color-change welcome-sign"
+            onClick={this.signIn}
+          >
+            Sign In <i className="fa fa-sign-in" />
+          </h6>
+        ) : (
+          <h6
+            className="cursor-color-change welcome-sign"
+            onClick={this.signOut}
+          >
+            Sign Out <i className="fa fa-sign-out" />
+          </h6>
+        )}
+      </div>
+    );
   }
- }
- export default User;
+}
+
+export default User;
